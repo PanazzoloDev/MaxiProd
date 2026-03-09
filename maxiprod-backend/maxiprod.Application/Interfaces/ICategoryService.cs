887cdd17c3ab14d0a80f3cb6509f7ad2d1ce6e6b
Panazzolo <wesleyosamigos@gmail.com>
2026-03-09 05:24:13 -0300

    Criado o gitignore e ajustado, removendo arquivos desnecessários

--- maxiprod-backend/maxiprod.Application/Interfaces/ICategoryService.cs
+++
@@ -1,9 +0,0 @@
-﻿using maxiprod.Application.DTOs;
-
-namespace maxiprod.Application.Interfaces
-{
-    public interface ICategoryService : IServiceBase<CreateCategoryDTO, UpdateCategoryDTO, ViewCategoryDTO>
-    {
-        /// Métodos especificos para o servico devem ser declarados aqui para o servico de Injecao
-    }
-}
--- maxiprod-frontend/src/Components/Datagrid/Components/SearchInput/styled.ts
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-const SearchInputContainer = styled.div<{children: React.ReactNode}>`
-    height: 10px;
-`
-export {
-    SearchInputContainer
-}
